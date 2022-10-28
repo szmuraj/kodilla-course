e942ee9444cf2b9e7e9ae41c038c8c151e26422d
szmuraj <przemyslaw.sicinski@o2.pl>
2022-10-28 12:01:15 +0200

    6.3

--- kodilla-testing/kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,10 +1,13 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbersExterminator {
-
+    
+    ArrayList <Integer> List = new ArrayList<>();
     public List<Integer> exterminate(List<Integer> numbers) {
         return null;
     }
+
 }
--- kodilla-testing/kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -3,6 +3,8 @@ package com.kodilla.testing.collection;
 import com.kodilla.testing.user.SimpleUser;
 import org.junit.jupiter.api.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionTestSuite {
@@ -21,11 +23,32 @@ public class CollectionTestSuite {
             "THEN getOddNumbersList should return null")
     @Test
     void testOddNumbersExterminatorEmptyList() {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+
+        //When
+        oddNumbersExterminator.exterminate(null);
+
+        //Then
+        Assertions.assertEquals((Integer) null,null);
     }
     @DisplayName("WHEN odd and even numbers are in the List, " +
             "THEN List should behave normaly")
     @Test
     void testOddNumbersExterminatorNormalList() {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+
+        //When
+        ArrayList<Integer> List = new ArrayList<>();
+        List.add(1);
+        List.add(2);
+        List.add(3);
+
+        oddNumbersExterminator.exterminate(List);
+
+        //Then
+        Assertions.assertEquals(List, List);
 
     }
 }
