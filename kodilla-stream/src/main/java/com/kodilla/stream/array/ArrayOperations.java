8b287f6757f6eded26819b613ee48bda737e5ae2
szmuraj <przemyslaw.sicinski@o2.pl>
2022-11-02 18:28:30 +0100

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,13 +1,14 @@
 package com.kodilla.stream.array;
 
-import java.util.OptionalDouble;
 import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.range;
 
 public interface ArrayOperations {
 
     static double getAverage(int numbers) {
         double avg=0.0;
-        System.out.println(IntStream.range(0,numbers));
-        return IntStream.range(0, numbers).average();
+        System.out.println(range(0,numbers));
+        double IntStream.range(0, numbers).average();
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -3,8 +3,11 @@ package com.kodilla.stream.world;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.kodilla.stream.world.Continent.europe;
+import static com.kodilla.stream.world.Continent.northAmerica;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WorldTestSuite {
@@ -12,15 +15,20 @@ public class WorldTestSuite {
     @Test
     void testGetPeopleQuantity() {
         //Given;
-        World world = new World();
+        List<List<Country>> earth = new ArrayList<>();
+        earth.add(europe);
+        earth.add(northAmerica);
+        europe.add(Country.poland);
+        europe.add(Country.germany);
+        northAmerica.add(Country.usa);
 
         //When
         BigDecimal peoplePopulation = BigDecimal.ZERO;
-        for (List<Country> continents : world.getEarth())
+        for (List<Country> continents : earth)
             for (Country country : continents) peoplePopulation = peoplePopulation.add(country.getPeopleQuantity());
 
         //Then
-        BigDecimal expected = new BigDecimal("211111110903703703670");
+        BigDecimal expected = new BigDecimal("445500000");
         assertEquals(expected, peoplePopulation);
     }
 }
