af70efa583d44f7e1c29acd69326e389df587751
szmuraj <przemyslaw.sicinski@o2.pl>
2022-12-02 14:09:44 +0100

    13.2

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderService.java
@@ -0,0 +1,42 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.time.LocalDateTime;
+
+public class OrderService {
+    public class OrderDTO {
+        private User user;
+        private boolean isOrdered;
+
+        public OrderDTO(final User user, final boolean isOrdered) {
+            this.user = user;
+            this.isOrdered = isOrdered;
+        }
+    }
+    public boolean order(final User user,final double amount, final Product product, final Seller seller, final DeliveryPlace deliveryPlace, final LocalDateTime orderDate) {
+        System.out.println("Ordering " + amount + product +  " for: " + user.getName() + user.getSurname()
+                + " to " + deliveryPlace + " at " + orderDate + ". ");
+
+        return true;
+    }
+
+    public class OrderRequest {
+        public User getUser() {
+            return user;
+        }
+        public double getAmount() {
+            return amount;
+        }
+        public Product getProduct() {
+            return getProduct;
+        }
+        public Seller getSeller() {
+            return seller;
+        }
+        public DeliveryPlace getDeliveryPlace() {
+            return deliveryPlace;
+        }
+        public LocalDateTime getOrderDate() {
+            return orderDate;
+        }
+    }
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -0,0 +1,29 @@
+package com.kodilla.good.patterns.challenges;
+
+import com.kodilla.good.patterns.challenges.OrderService.OrderDTO;
+import com.kodilla.good.patterns.challenges.OrderService.OrderRequest;
+public class ProductOrderService {
+    private InformationService informationService;
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+
+    public ProductOrderService(final InformationService informationService, final OrderService orderService, final OrderRepository orderRepository) {
+        this.informationService = informationService;
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+    }
+
+    public OrderDTO process(final OrderRequest orderRequest) {
+        boolean isOrdered = orderService.order(orderRequest.getUser(), orderRequest.getAmount(), orderRequest.getProduct(), orderRequest.getSeller(), orderRequest.getDeliveryPlace(), orderRequest.getOrderDate());
+
+        if (isOrdered) {
+            informationService.inform(orderRequest.getUser(), orderRequest.getProduct);
+            orderRepository.createOrder(orderRequest.getUser(), orderRequest.getProduct(), orderRequest.getSeller(), orderRequest.getDeliveryPlace, orderRequest.amount);
+            orderDelivery;
+
+            return new OrderDTO(orderRequest.getUser(), true);
+        } else {
+            return new OrderDTO(orderRequest.getUser(), false);
+        }
+    }
+}
