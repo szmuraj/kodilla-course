a0bfba65243ebf779a167c6aa0b061b16a8cb3b7
szmuraj <przemyslaw.sicinski@o2.pl>
2023-01-21 15:24:24 +0100

    17.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,39 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String LISTNAME = "Test: ListName";
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(listName);
+
+        //Then
+        assertEquals(1, readTaskList.size());
+
+        //CleanUp
+        //int id = readTaskList.get(0).getId();
+        //taskListDao.deleteById(id);
+        taskListDao.deleteAll();
+
+    }
+}
